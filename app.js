a9f9923ef110a61cbdfc01692c43aeedadd573d5
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-19 00:19:24 +0200

    use express.urlencoded to retrieve input values from signup html page + write correpsonding post method

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require("express")
 
 const app = express()
 app.use(express.static('public'));
-
-
+app.use(express.urlencoded({extended: true}))
 const port = 3000
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/html/signup.html")
+})
+
+app.post("/", function(req, res){
+    console.log("request on '/' url")
+    let firstName = req.body.firstName
+    let lastName = req.body.lastName
+    let email = req.body.email
+    console.log("user first name : " + firstName + "\nuser last name : " + lastName +"\nuser mail : " + email)
 })
 
 app.listen(port, function(){
