4739051cf7706927c655f1e2ada806cde16b03a2
Maxime Traverse <maxime.Traverse@orange.fr>
2021-08-19 00:01:10 +0200

    send signup.html page as home page with get("/") method + use express.static

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express")
 
 const app = express()
+app.use(express.static('public'));
+
+
 const port = 3000
 
 app.get("/", function(req, res) {
-    res.send("<h1>NewsLetter Test</h1>")
+    res.sendFile(__dirname + "/public/html/signup.html")
 })
 
 app.listen(port, function(){
