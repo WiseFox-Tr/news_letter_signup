9897fc5f98d70ac669ef66834919cdba828a0ee4
Maxime Traverse <maxime.Traverse@orange.fr>
2021-09-09 17:33:55 +0200

    require dotenv package and test access to .env file values

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
 
 const app = express()
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}))
-const port = 3000
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/public/html/signup.html")
@@ -17,6 +18,11 @@ app.post("/", function(req, res){
     console.log("user first name : " + firstName + "\nuser last name : " + lastName +"\nuser mail : " + email)
 })
 
+const port = process.env.PORT
 app.listen(port, function(){
     console.log("Server started on port " + port)
 })
+
+const API_KEY = process.env.API_KEY
+const AUDIENCE_LIST_ID = process.env.AUDIENCE_LIST_ID
+console.log("API KEY = " + API_KEY + "\nAUDIENCE ID = " + AUDIENCE_LIST_ID)
