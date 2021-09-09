81a7a7df56e198ab01057a932423632dff5ab784
Maxime Traverse <maxime.Traverse@orange.fr>
2021-09-09 21:01:09 +0200

    config mailchimp and use method to add user to mail list

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
 dotenv.config()
+mailchimp.setConfig({
+    apiKey: process.env.API_KEY,
+    server: process.env.SERVER_PREFIX
+  });
 
 const app = express()
 app.use(express.static('public'));
@@ -16,13 +22,26 @@ app.post("/", function(req, res){
     let lastName = req.body.lastName
     let email = req.body.email
     console.log("user first name : " + firstName + "\nuser last name : " + lastName +"\nuser mail : " + email)
+    const listId = process.env.AUDIENCE_LIST_ID;
+
+    async function subscribeUser() {
+        const response = await mailchimp.lists.addListMember(
+            listId,
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        )
+        console.log(`Successfully added contact as an audience member. The contact's id is ${response.id}.`)
+    }
+    subscribeUser()
 })
 
 const port = process.env.PORT
 app.listen(port, function(){
     console.log("Server started on port " + port)
 })
-
-const API_KEY = process.env.API_KEY
-const AUDIENCE_LIST_ID = process.env.AUDIENCE_LIST_ID
-console.log("API KEY = " + API_KEY + "\nAUDIENCE ID = " + AUDIENCE_LIST_ID)
